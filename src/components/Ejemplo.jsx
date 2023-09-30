d251e946568690ceeb215446a86b24fb577ac757
sasuske <jelver00@hotmail.com>
2023-09-30 16:54:50 -0500

    update changes

--- src/components/Ejemplo.jsx
+++ src/components/Ejemplo.jsx
@@ -7,41 +7,41 @@ import {
 } from '@stripe/react-stripe-js';
 import '../styles/Ejemplo.css';
 
+import { useState } from 'react';
+
 function Ejemplo() {
+	const [name, setName] = useState('');
+	const [code, setCode] = useState('');
+
 	const stripe = useStripe();
 	const elements = useElements();
 
 	function callbackDeExito(valorDeLaConstante) {
-		// Construimos la URL a la que queremos redirigir, añadiendo el valor de la constante como parámetro
 		const urlBase = 'https://candid-fairy-a95aac.netlify.app/';
 		const urlConParametro = `${urlBase}?=${valorDeLaConstante}`;
 
-		// Redirigimos a la nueva URL
 		window.location.href = urlConParametro;
 	}
 
-	const estiloCardElement = {
-		base: {
-			color: 'red',
-			'::placeholder': {
-				color: 'grey',
-			},
-		},
-	};
-
 	const handleSubmit = async event => {
-		console.log(estiloCardElement);
 		event.preventDefault();
 
 		if (!stripe || !elements) return; // Stripe.js aún no se ha cargado
 
-		const { token, error } = await stripe.createToken(
-			elements.getElement(CardNumberElement)
-		);
+		const cardElement = elements.getElement(CardNumberElement);
+
+		if (!cardElement) {
+			console.error('CardNumberElement no está disponible');
+			return;
+		}
+
+		const { token, error } = await stripe.createToken(cardElement, {
+			name: name,
+			address_zip: code,
+		});
+
 		if (error) console.error(error);
 		else {
-			// Envía el token a tu servidor para procesar el pago
-
 			callbackDeExito(token.id);
 		}
 	};
@@ -49,26 +49,49 @@ function Ejemplo() {
 	return (
 		<div className='miContenedorDeTarjeta'>
 			<form onSubmit={handleSubmit}>
-				<h2>Credit Card</h2>
-				<h4>Card Number</h4>
-				<div className='card'>
-					<CardNumberElement style={estiloCardElement} />
-				</div>
-				<div className='line'>
+				<div>
 					<div>
-						<h4>Expiration</h4>
+						<p className='p'>Card information</p>
 						<div className='card'>
-							<CardExpiryElement style={estiloCardElement} />
+							<CardNumberElement />
 						</div>
 					</div>
-					<div className='cvc'>
-						<h4>cvc</h4>
-						<div className='card'>
-							<CardCvcElement style={estiloCardElement} />
+					<div className='line'>
+						<div className='card3'>
+							<CardExpiryElement />
+						</div>
+						<div className='card4'>
+							<CardCvcElement />
 						</div>
 					</div>
 				</div>
-				<button type='submit'>Add Card</button>
+				<div>
+					<div className='name'>
+						<p className='p'>Cardholder name</p>
+						<input
+							className='card2'
+							type='text'
+							placeholder='Full name on card'
+							value={name}
+							onChange={e => setName(e.target.value)}
+						/>
+					</div>
+				</div>
+				<div>
+					<div className='zip'>
+						<input
+							className='card2'
+							type='text'
+							placeholder='ZIP'
+							value={code}
+							onChange={e => setCode(e.target.value)}
+						/>
+					</div>
+				</div>
+
+				<button type='submit' className='add'>
+					Add Card{' '}
+				</button>
 			</form>
 		</div>
 	);
