5d4fe743c81e091f5d28a51ec7617d106081ecd0
sasuske <jelver00@hotmail.com>
2023-09-30 10:44:55 -0500

    update changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import PaymentForm from './components/ PaymentForm';
-import './App.css';
-import MiTarjeta from './components/Prueba';
 import Ejemplo from './components/Ejemplo';
+import './App.css';
 
-const stripePromiseDev = loadStripe('process.env.REACT_APP_DEVELOPMENT_KEY');
-const stripePromisePro = loadStripe('process.env.REACT_APP_PRODUCTION_KEY');
+// Aquí, deberías acceder al valor de la variable de entorno, no pasar una cadena de texto.
+const stripePromise = loadStripe(process.env.REACT_APP_DEVELOPMENT_KEY);
+/* const stripePromisePro = loadStripe(process.env.REACT_APP_PRODUCTION_KEY); */
+
 function App() {
 	return (
 		<div className='app'>
 			<h1>Add method payment</h1>
-			<Elements stripe={stripePromiseDev}>
-				{/* 	<PaymentForm /> */}
-				{/* <MiTarjeta /> */}
+			<Elements stripe={stripePromise}>
 				<Ejemplo />
 			</Elements>
 		</div>
