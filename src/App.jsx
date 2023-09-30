39b6a6911a87590bd68f20c35d527edb4e98ba9e
sasuske <jelver00@hotmail.com>
2023-09-30 10:52:37 -0500

    update changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import Ejemplo from './components/Ejemplo';
 import './App.css';
 
 // Aquí, deberías acceder al valor de la variable de entorno, no pasar una cadena de texto.
-const stripePromise = loadStripe(process.env.REACT_APP_DEVELOPMENT_KEY);
+
+const stripePromiseDev = loadStripe(
+	import.meta.env.VITE_REACT_APP_DEVELOPMENT_KEY
+);
 /* const stripePromisePro = loadStripe(process.env.REACT_APP_PRODUCTION_KEY); */
 
 function App() {
 	return (
 		<div className='app'>
 			<h1>Add method payment</h1>
-			<Elements stripe={stripePromise}>
+			<Elements stripe={stripePromiseDev}>
 				<Ejemplo />
 			</Elements>
 		</div>
