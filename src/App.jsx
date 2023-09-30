d7da60ce6cd64fb3dbfe75607af0045c4f989bd9
sasuske <jelver00@hotmail.com>
2023-09-30 09:57:38 -0500

    update changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ import './App.css';
 import MiTarjeta from './components/Prueba';
 import Ejemplo from './components/Ejemplo';
 
-const stripePromise = loadStripe(
+const stripePromiseDev = loadStripe(
 	'pk_test_51NqbCjE2i202Okp0cokZjItpc9YZ6y6BOm1TVCvPd74rW408etPgmJBVTYoK9QStzmGkahBsJctCXYQVLD9dFtiM006XAOVzfP'
 );
+
+const stripePromisePro = loadStripe(
+	"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"
 
 function App() {
 	return (
